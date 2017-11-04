5b3dc7cdc7e87af85f647ebc9e660f2026ebfcde
HideakiTouhara <hideee6723@gmail.com>
2017-11-04 15:29:53 +0900

    Set up for ARKit

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+//        earth.geometry?.firstMaterial?.diffuse.contents = 
     }
 
     override func didReceiveMemoryWarning() {
