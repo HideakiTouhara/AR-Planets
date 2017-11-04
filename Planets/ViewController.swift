6e6a439ab6e2ece424cb90e15be7448a0809a42e
HideakiTouhara <hideee6723@gmail.com>
2017-11-04 16:10:52 +0900

    Add sun object

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -23,18 +23,18 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.35))
+        sun.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun diffuse")
+        sun.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(sun)
         let earth = SCNNode()
         earth.geometry = SCNSphere(radius: 0.2)
         earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Earth day")
         earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "Earth Specular")
         earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "Earth Emission")
         earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "Earth Normal")
-        earth.position = SCNVector3(0,0,-1)
-        self.sceneView.scene.rootNode.addChildNode(earth)
-        
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
-        let forever = SCNAction.repeatForever(action)
-        earth.runAction(forever)
+        earth.position = SCNVector3(1.2,0,0)
+        sun.addChildNode(earth)
     }
 
     override func didReceiveMemoryWarning() {
